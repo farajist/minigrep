f8e3fa0ebd9c15f18a3f44ed0728a0ceb987d5b7
Hamza Faraji <farajihamza18@gmail.com>
2024-05-31 12:32:33 +0100

    fix: write errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|_| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(_) = minigrep::run(config) {
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
